3a3570ae23d4f4ed03e47d2f8424d356be9229b8
bowbow0807 <bowbow19@gmail.com>
2021-10-10 23:43:38 +0800

    路徑串聯完成
    1.可顯示全部的商品及內容
    2.可顯示詳細餐廳資訊
    3.可按照餐廳名稱及類別搜尋

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const exhbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
 const app = express()
 const port = 3000
 
-app.engine('handlebars', exhbs({defaultLayout : 'main'}))
+app.engine('handlebars', exphbs({defaultLayout : 'main'}))
 app.set('view engine' , 'handlebars')
 
 app.use(express.static('public'))
 
 app.get('/',(req,res) =>{
 
-    res.render('index' , {restaurant : restaurantList})
+    res.render('index' , {restaurant : restaurantList.results})
 })
 
-app.get('/show',(req,res) =>{
-    res.render('show')
+app.get('/search',(req,res) =>{
+    console.log('search' , req.query)
+    const keyword = req.query.keyword
+    const pages = restaurantList.results.filter(item => {
+        return  item.name.toLowerCase().includes(keyword.toLowerCase()) || item.category.toLowerCase().includes(keyword.toLowerCase())
+    })
+    res.render('index' , {restaurant : pages, keyword : keyword})
+})
+
+app.get('/restaurants/:page_id',(req,res) =>{
+    const restaurantOne = restaurantList.results.find(item => 
+        item.id.toString() === req.params.page_id
+    )
+    res.render('show' , {restanrants : restaurantOne})
 })
 app.listen(port , () =>{
     console.log(`Express is listening in localhost:${port}`);
